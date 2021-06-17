e33dbf359211824562e5745505e4c26ab040e2ca
Taichi I <fng.trf.kk1020@gmail.com>
2021-06-16 18:23:28 -0700

    first commit

---
+++ src/router/AppRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import MainPage from '../component/MainPage';
+import MyPage from '../component/MyPage';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={MainPage} exact={true} />
+      <Route path="/mypage" component={MyPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default AppRouter;
