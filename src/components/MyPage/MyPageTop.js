b11dba07d5d20fd44bb4f0eb78a7d74995c11b81
Taichi I <fng.trf.kk1020@gmail.com>
2021-06-18 02:33:07 -0700

    added modal for posting

--- src/components/MyPage/MyPageTop.js
+++ src/components/MyPage/MyPageTop.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MyPageTop.css';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
 import SettingsIcon from '@material-ui/icons/Settings';
+import Modal from '@material-ui/core/Modal';
 
 const MyPageTop = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const onPhotoChange = () => {
+    handleClose();
+  }
+
   return (
-    <>
+    <div className="my-page-top">
       <div className="user-info-wrapper">
         <div className="icon-wrapper">
           <div className="icon">
@@ -26,11 +41,34 @@ const MyPageTop = () => {
         </div>
       </div>
       <div className="post-button">
-        <Button variant="contained" color="primary">
+        <Button onClick={handleOpen} variant="contained" color="primary">
           Post
         </Button>
       </div>
-    </>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="post-modal-title"
+        aria-describedby="post-modal-description"
+      >
+        <div className="modal">
+          <div className="post-modal-title-wrapper">
+            <h2 id="post-modal-title">New Post</h2>
+          </div>
+          <div className="posting-area">
+            <Button component="label" variant="contained" color="primary">
+              Post a photo
+              <input 
+                type="file"
+                onChange={onPhotoChange}
+                accept="image/*"
+                hidden
+              />
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </div>
   )
 };
 
