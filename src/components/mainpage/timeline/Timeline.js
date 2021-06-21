627d31859102365201d9df64a910b65fe33b7528
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-06-20 19:27:30 -0700

    delete unnecessary import

--- src/components/mainpage/timeline/Timeline.js
+++ src/components/mainpage/timeline/Timeline.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { usePhotosContext } from "../../../context/PhotosProvider";
 import TimelineItem from "./TimelineItem";
-import "./Timeline.css";
 
 const Timeline = () => {
   const { photosData } = usePhotosContext();
