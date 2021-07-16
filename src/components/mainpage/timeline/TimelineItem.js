84624fd9cbb4d926460219f57c742b8d36325fdc
Taichi I <fng.trf.kk1020@gmail.com>
2021-07-15 20:45:46 -0700

    start responsive style

--- src/components/mainpage/timeline/TimelineItem.js
+++ src/components/mainpage/timeline/TimelineItem.js
@@ -10,7 +10,7 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 
-import "./TimelineItem.css";
+import "./TimelineItem.scss";
 
 // Material-UI
 const useStyles = makeStyles((theme) => ({
